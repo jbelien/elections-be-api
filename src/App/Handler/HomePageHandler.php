06254697d552e80781d7e122e43ec335a536ed90
jbelien <dev@jbelien.be>
2019-05-27 20:20:50 +0000

    Apply fixes from StyleCI

--- src/App/Handler/HomePageHandler.php
+++ src/App/Handler/HomePageHandler.php
@@ -7,10 +7,8 @@ namespace App\Handler;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Diactoros\Response\RedirectResponse;
 use Zend\Expressive\Router\RouterInterface;
-use Zend\Expressive\Template\TemplateRendererInterface;
 
 class HomePageHandler implements RequestHandlerInterface
 {
