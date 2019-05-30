62609c9372bee2b26eb87a5841ee4057bab49072
jbelien <dev@jbelien.be>
2019-05-30 13:41:00 +0000

    Apply fixes from StyleCI

--- src/App/Handler/API/EntitiesHandler.php
+++ src/App/Handler/API/EntitiesHandler.php
@@ -5,11 +5,11 @@ declare(strict_types=1);
 namespace App\Handler\API;
 
 use App\Reader\FormatI\Entity;
+use App\Reader\FormatI\Extension;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use Zend\Diactoros\Response\JsonResponse;
-use App\Reader\FormatI\Extension;
 
 class EntitiesHandler implements RequestHandlerInterface
 {
